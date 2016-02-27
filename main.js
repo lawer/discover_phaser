d7b43d163672fbb2cf5c065c26cc680cff089f0b
lawer <carlesgm@gmail.com>
2016-02-27 21:51:12 +0100

    Refactorizamos

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var GameModule;
 (function (GameModule) {
-    var game;
+    var Point = Phaser.Point;
     var mainState = (function (_super) {
         __extends(mainState, _super);
         function mainState() {
@@ -15,57 +15,57 @@ var GameModule;
         mainState.prototype.preload = function () {
             _super.prototype.preload.call(this);
             // Precargamos los sprites
-            game.load.image("player", "assets/player.png");
-            game.load.image('paredV', 'assets/wallVertical.png');
-            game.load.image('paredH', 'assets/wallHorizontal.png');
-            game.load.image('moneda', 'assets/coin.png');
+            this.game.load.image("player", "assets/player.png");
+            this.game.load.image('paredV', 'assets/wallVertical.png');
+            this.game.load.image('paredH', 'assets/wallHorizontal.png');
+            this.game.load.image('moneda', 'assets/coin.png');
         };
         mainState.prototype.crearMundo = function () {
             // Creamos un grupo para las paredes y les asignamos física
-            this.paredes = game.add.group();
+            this.paredes = this.game.add.group();
             this.paredes.enableBody = true;
-            game.add.sprite(0, 0, 'paredV', 0, this.paredes); // Izquierda
-            game.add.sprite(480, 0, 'paredV', 0, this.paredes); // Derecha
-            game.add.sprite(0, 0, 'paredH', 0, this.paredes); // Arriba Izquierda
-            game.add.sprite(300, 0, 'paredH', 0, this.paredes); // Arriba Derecha
-            game.add.sprite(0, 320, 'paredH', 0, this.paredes); // Abajo Izquierda
-            game.add.sprite(300, 320, 'paredH', 0, this.paredes); // Abajo Derecha
-            game.add.sprite(-100, 160, 'paredH', 0, this.paredes); // Centro Izquierda
-            game.add.sprite(400, 160, 'paredH', 0, this.paredes); // Centro Derecha
+            this.game.add.sprite(0, 0, 'paredV', 0, this.paredes); // Izquierda
+            this.game.add.sprite(480, 0, 'paredV', 0, this.paredes); // Derecha
+            this.game.add.sprite(0, 0, 'paredH', 0, this.paredes); // Arriba Izquierda
+            this.game.add.sprite(300, 0, 'paredH', 0, this.paredes); // Arriba Derecha
+            this.game.add.sprite(0, 320, 'paredH', 0, this.paredes); // Abajo Izquierda
+            this.game.add.sprite(300, 320, 'paredH', 0, this.paredes); // Abajo Derecha
+            this.game.add.sprite(-100, 160, 'paredH', 0, this.paredes); // Centro Izquierda
+            this.game.add.sprite(400, 160, 'paredH', 0, this.paredes); // Centro Derecha
             // Escalamos las paredes para usar todo el espacio
-            var centroArriba = game.add.sprite(100, 80, 'paredH', 0, this.paredes);
+            var centroArriba = this.game.add.sprite(100, 80, 'paredH', 0, this.paredes);
             centroArriba.scale.setTo(1.5, 1);
-            var centroAbajo = game.add.sprite(100, 240, 'paredH', 0, this.paredes);
+            var centroAbajo = this.game.add.sprite(100, 240, 'paredH', 0, this.paredes);
             centroAbajo.scale.setTo(1.5, 1);
             // Set all the walls to be immovable
             this.paredes.setAll('body.immovable', true);
         };
         mainState.prototype.create = function () {
             _super.prototype.create.call(this);
-            game.stage.backgroundColor = "#3498db";
-            game.physics.startSystem(Phaser.Physics.ARCADE);
+            this.game.stage.backgroundColor = "#3498db";
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
             /*
              Para situar al personaje en el centro de la escena utilizamos variables predefinidas
-             Otras útiles son game.world.width, game.world.height, game.world.randomX,
-             game.world.randomY
+             Otras útiles son this.game.world.width, this.game.world.height, this.game.world.randomX,
+             this.game.world.randomY
              */
-            this.player = this.game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+            this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
             // Cambiamos el "anchor" del jugador
             this.player.anchor.setTo(0.5, 0.5);
             // Le decimos a Phaser que el usuario usará el motor de físicas Arcade
-            game.physics.arcade.enable(this.player);
+            this.game.physics.arcade.enable(this.player);
             // Agregamos gravedad al jugador
             this.player.body.gravity.y = 500;
             // Cogemos los cursores para gestionar la entrada
-            this.cursor = game.input.keyboard.createCursorKeys();
+            this.cursor = this.game.input.keyboard.createCursorKeys();
             this.crearMundo();
             // Muestra la moneda
-            this.moneda = game.add.sprite(60, 140, 'moneda');
-            game.physics.arcade.enable(this.moneda);
+            this.moneda = this.game.add.sprite(60, 140, 'moneda');
+            this.game.physics.arcade.enable(this.moneda);
             // Cambiamos el "anchor" de la moneda al centro
             this.moneda.anchor.setTo(0.5, 0.5);
             // Muestra la puntuación
-            this.etiquetaPuntos = game.add.text(30, 30, 'puntos: 0', { font: '18px Arial', fill: '#ffffff' });
+            this.etiquetaPuntos = this.game.add.text(30, 30, 'puntos: 0', { font: '18px Arial', fill: '#ffffff' });
             // Incializa la variable con la puntuación
             this.puntos = 0;
         };
@@ -93,21 +93,41 @@ var GameModule;
         mainState.prototype.update = function () {
             _super.prototype.update.call(this);
             // Activamos las colisiones entre el jugador y las paredes
-            game.physics.arcade.collide(this.player, this.paredes);
+            this.game.physics.arcade.collide(this.player, this.paredes);
             this.movePlayer();
             if (!this.player.inWorld) {
                 this.muerte();
             }
-            game.physics.arcade.overlap(this.player, this.moneda, this.cogerMoneda, null, this);
+            this.game.physics.arcade.overlap(this.player, this.moneda, this.cogerMoneda, null, this);
         };
         mainState.prototype.muerte = function () {
-            game.state.start('main');
+            this.game.state.start('main');
         };
         ;
+        mainState.prototype.cambiaPosicionMoneda = function () {
+            // Creamos un array con todas las posibles posiciones que podrà tomar la moneda
+            var posiciones = [
+                new Point(140, 60), new Point(360, 60),
+                new Point(60, 140), new Point(440, 140),
+                new Point(130, 300), new Point(370, 300),
+            ];
+            // Quitamos la posición actual de las disponibles en el Array
+            // Si no lo hicieramos al ser aleatorio la moneda podría aparecer en la misma posición otra vez.
+            // Aprovechamos el hecho que no se repite nunca la "x"
+            for (var i = 0; i < posiciones.length; i++) {
+                if (posiciones[i].x === this.moneda.x) {
+                    // La función "splice" toma 2 parametros:
+                    // la posición inicial y el número de elementos a eliminar partiendo de dicha posición.
+                    posiciones.splice(i, 1);
+                }
+            }
+            // Elegimos la nueva posición aleatoriamente.
+            var newPosition = this.game.rnd.pick(posiciones);
+            // Situamos la moneda en la nueva posición.
+            this.moneda.reset(newPosition.x, newPosition.y);
+        };
         mainState.prototype.cogerMoneda = function (jugador, moneda) {
-            var newX = game.rnd.integerInRange(10, game.world.width - 20);
-            var newY = game.rnd.integerInRange(10, game.world.height - 20);
-            this.moneda.reset(newX, newY);
+            this.cambiaPosicionMoneda();
             // Incrementamos la puntuación
             this.puntos += 5;
             // Actualizamos la etiqueta con la puntuación
@@ -117,9 +137,9 @@ var GameModule;
     })(Phaser.State);
     var SimpleGame = (function () {
         function SimpleGame() {
-            game = new Phaser.Game(500, 340, Phaser.AUTO, "gameDiv");
-            game.state.add("main", mainState);
-            game.state.start("main");
+            this.game = new Phaser.Game(500, 340, Phaser.AUTO, "gameDiv");
+            this.game.state.add("main", mainState);
+            this.game.state.start("main");
         }
         return SimpleGame;
     })();
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /// <reference path="phaser/phaser.d.ts"/>
 
 module GameModule {
-    var game: Phaser.Game;
+    import Point = Phaser.Point;
 
     class mainState extends Phaser.State {
         player: Phaser.Sprite;
@@ -16,32 +16,32 @@ module GameModule {
             super.preload();
 
             // Precargamos los sprites
-            game.load.image("player", "assets/player.png");
-            game.load.image('paredV', 'assets/wallVertical.png');
-            game.load.image('paredH', 'assets/wallHorizontal.png');
-            game.load.image('moneda', 'assets/coin.png');
+            this.game.load.image("player", "assets/player.png");
+            this.game.load.image('paredV', 'assets/wallVertical.png');
+            this.game.load.image('paredH', 'assets/wallHorizontal.png');
+            this.game.load.image('moneda', 'assets/coin.png');
         }
 
         crearMundo():void {
             // Creamos un grupo para las paredes y les asignamos física
-            this.paredes = game.add.group();
+            this.paredes = this.game.add.group();
             this.paredes.enableBody = true;
 
-            game.add.sprite(0, 0, 'paredV', 0, this.paredes); // Izquierda
-            game.add.sprite(480, 0, 'paredV', 0, this.paredes); // Derecha
+            this.game.add.sprite(0, 0, 'paredV', 0, this.paredes); // Izquierda
+            this.game.add.sprite(480, 0, 'paredV', 0, this.paredes); // Derecha
 
-            game.add.sprite(0, 0, 'paredH', 0, this.paredes); // Arriba Izquierda
-            game.add.sprite(300, 0, 'paredH', 0, this.paredes); // Arriba Derecha
-            game.add.sprite(0, 320, 'paredH', 0, this.paredes); // Abajo Izquierda
-            game.add.sprite(300, 320, 'paredH', 0, this.paredes); // Abajo Derecha
+            this.game.add.sprite(0, 0, 'paredH', 0, this.paredes); // Arriba Izquierda
+            this.game.add.sprite(300, 0, 'paredH', 0, this.paredes); // Arriba Derecha
+            this.game.add.sprite(0, 320, 'paredH', 0, this.paredes); // Abajo Izquierda
+            this.game.add.sprite(300, 320, 'paredH', 0, this.paredes); // Abajo Derecha
 
-            game.add.sprite(-100, 160, 'paredH', 0, this.paredes); // Centro Izquierda
-            game.add.sprite(400, 160, 'paredH', 0, this.paredes); // Centro Derecha
+            this.game.add.sprite(-100, 160, 'paredH', 0, this.paredes); // Centro Izquierda
+            this.game.add.sprite(400, 160, 'paredH', 0, this.paredes); // Centro Derecha
 
             // Escalamos las paredes para usar todo el espacio
-            var centroArriba = game.add.sprite(100, 80, 'paredH', 0, this.paredes);
+            var centroArriba = this.game.add.sprite(100, 80, 'paredH', 0, this.paredes);
             centroArriba.scale.setTo(1.5, 1);
-            var centroAbajo = game.add.sprite(100, 240, 'paredH', 0, this.paredes);
+            var centroAbajo = this.game.add.sprite(100, 240, 'paredH', 0, this.paredes);
             centroAbajo.scale.setTo(1.5, 1);
 
             // Set all the walls to be immovable
@@ -50,42 +50,42 @@ module GameModule {
 
         create():void {
             super.create();
-            game.stage.backgroundColor = "#3498db";
-            game.physics.startSystem(Phaser.Physics.ARCADE);
+            this.game.stage.backgroundColor = "#3498db";
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
             /*
              Para situar al personaje en el centro de la escena utilizamos variables predefinidas
-             Otras útiles son game.world.width, game.world.height, game.world.randomX,
-             game.world.randomY
+             Otras útiles son this.game.world.width, this.game.world.height, this.game.world.randomX,
+             this.game.world.randomY
              */
             this.player = this.game.add.sprite(
-                game.world.centerX,
-                game.world.centerY,
+                this.game.world.centerX,
+                this.game.world.centerY,
                 'player');
 
             // Cambiamos el "anchor" del jugador
             this.player.anchor.setTo(0.5, 0.5);
 
             // Le decimos a Phaser que el usuario usará el motor de físicas Arcade
-            game.physics.arcade.enable(this.player);
+            this.game.physics.arcade.enable(this.player);
             // Agregamos gravedad al jugador
             this.player.body.gravity.y = 500;
 
             // Cogemos los cursores para gestionar la entrada
-            this.cursor = game.input.keyboard.createCursorKeys();
+            this.cursor = this.game.input.keyboard.createCursorKeys();
 
             this.crearMundo();
 
             // Muestra la moneda
-            this.moneda = game.add.sprite(60, 140, 'moneda');
+            this.moneda = this.game.add.sprite(60, 140, 'moneda');
 
-            game.physics.arcade.enable(this.moneda);
+            this.game.physics.arcade.enable(this.moneda);
 
             // Cambiamos el "anchor" de la moneda al centro
             this.moneda.anchor.setTo(0.5, 0.5);
 
             // Muestra la puntuación
-            this.etiquetaPuntos = game.add.text(30, 30, 'puntos: 0', { font: '18px Arial', fill: '#ffffff' });
+            this.etiquetaPuntos = this.game.add.text(30, 30, 'puntos: 0', { font: '18px Arial', fill: '#ffffff' });
             // Incializa la variable con la puntuación
             this.puntos = 0;
         }
@@ -119,25 +119,48 @@ module GameModule {
             super.update();
 
             // Activamos las colisiones entre el jugador y las paredes
-            game.physics.arcade.collide(this.player, this.paredes);
+            this.game.physics.arcade.collide(this.player, this.paredes);
             this.movePlayer();
 
             if (!this.player.inWorld) {
                 this.muerte();
             }
 
-            game.physics.arcade.overlap(this.player, this.moneda, this.cogerMoneda, null, this);
+            this.game.physics.arcade.overlap(this.player, this.moneda, this.cogerMoneda, null, this);
         }
 
         private muerte() {
-            game.state.start('main');
+            this.game.state.start('main');
         };
 
-        private cogerMoneda(jugador:Phaser.Sprite, moneda:Phaser.Sprite) {
-            var newX = game.rnd.integerInRange(10, game.world.width - 20);
-            var newY = game.rnd.integerInRange(10, game.world.height - 20);
+        private cambiaPosicionMoneda() {
+            // Creamos un array con todas las posibles posiciones que podrà tomar la moneda
+            var posiciones: Point[] = [
+                new Point(140, 60), new Point(360, 60), // Fila arriba
+                new Point(60, 140), new Point(440, 140), // Fila central
+                new Point(130, 300), new Point(370, 300), // Fila abajo
+            ];
 
-            this.moneda.reset(newX, newY);
+            // Quitamos la posición actual de las disponibles en el Array
+            // Si no lo hicieramos al ser aleatorio la moneda podría aparecer en la misma posición otra vez.
+            // Aprovechamos el hecho que no se repite nunca la "x"
+            for (var i = 0; i < posiciones.length; i++) {
+                if (posiciones[i].x === this.moneda.x) {
+                    // La función "splice" toma 2 parametros:
+                    // la posición inicial y el número de elementos a eliminar partiendo de dicha posición.
+                    posiciones.splice(i, 1);
+                }
+            }
+
+            // Elegimos la nueva posición aleatoriamente.
+            var newPosition = this.game.rnd.pick(posiciones);
+
+            // Situamos la moneda en la nueva posición.
+            this.moneda.reset(newPosition.x, newPosition.y);
+        }
+
+        private cogerMoneda(jugador:Phaser.Sprite, moneda:Phaser.Sprite) {
+            this.cambiaPosicionMoneda();
 
             // Incrementamos la puntuación
             this.puntos += 5;
@@ -148,12 +171,13 @@ module GameModule {
     }
 
     export class SimpleGame {
+        game:Phaser.Game;
 
         constructor() {
-            game = new Phaser.Game(500, 340, Phaser.AUTO, "gameDiv");
+            this.game = new Phaser.Game(500, 340, Phaser.AUTO, "gameDiv");
 
-            game.state.add("main", mainState);
-            game.state.start("main");
+            this.game.state.add("main", mainState);
+            this.game.state.start("main");
         }
     }
 }
